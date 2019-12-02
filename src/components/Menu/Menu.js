3e23eab71158a04e3633ee9760e32f3dffecc9c1
Pat Haynes <pathaynes32@gmail.com>
2019-12-02 09:32:08 -0800

    updated menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,14 +3,13 @@ import styles from './Menu.css';
 import { Link } from 'react-router-dom';
 
 const Menu = () => (
-  <header>
-    <nav className={styles.Menu}>
-      <Link to='/'>HOME</Link>
-      <Link to='/about'>ABOUT</Link>
-      <Link to='/projects'>PROJECTS</Link>
-      <Link to='/contact'>CONTACT</Link>
-    </nav>
-  </header>
+  <ul className={styles.Menu}>
+    <li><Link to='/'>HOME</Link></li>
+    <li><Link to='/about'>ABOUT</Link></li>
+    <li><Link to='/projects'>PROJECTS</Link></li>
+    <li><Link to='/contact'>CONTACT</Link></li>
+  </ul>
+
 );
 
 export default Menu;
