e406733b4cd03583976e92f532ed5bffb488bbc7
Pat Haynes <pathaynes32@gmail.com>
2019-12-31 19:21:09 -0800

    checking to see if github is saving

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,8 @@ import styles from './About.css';
 const About = () => (
   <>
     <div className={styles.About}>
-      <p>Here is where I talk about myself!</p>
+      <p>Here is where I write something really cool!</p>
+      <img>this is where a cool image goes</img>
     </div>
   </>
 );
