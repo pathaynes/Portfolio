514295be333f573a8a0ddf6f46a3829598cd8c6a
Pat Haynes <pathaynes32@gmail.com>
2019-12-31 19:22:59 -0800

    updating About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,6 +6,9 @@ const About = () => (
     <div className={styles.About}>
       <p>Here is where I write something really cool!</p>
       <img>this is where a cool image goes</img>
+      <p>How about we add some more paragraphs</p>
+      <p>How about we add some more paragraphs</p>
+      <p>How about we add some more paragraphs</p>
     </div>
   </>
 );
