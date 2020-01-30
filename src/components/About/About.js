fbb333090c553597dc37f22fccb61ef2f6c401b7
Pat Haynes <pathaynes32@gmail.com>
2020-01-29 16:12:33 -0800

    updated my photo

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,9 +14,9 @@ const About = () => (
       <div className={styles.HeadShot} data-aos="zoom-in">
         <img src={Pat}></img>
         <ul className={styles.Social}>
-          <li><a href='http://www.linkedin.com/in/pat-haynes/' target='blank'><FontAwesomeIcon icon={faLinkedinIn} /><span>linkedin profile</span></a></li>
-          <li><a href='https://github.com/pathaynes' target='blank'><FontAwesomeIcon icon={faGithub} /><span>github profile</span></a></li>
-          <li><a href='https://www.instagram.com/pat_haynes_/' target='blank'><FontAwesomeIcon icon={faInstagram} /><span>instagram account</span></a></li>
+          <li><a href='http://www.linkedin.com/in/pat-haynes/' target='blank'><FontAwesomeIcon icon={faLinkedinIn} /><span>linkedin</span></a></li>
+          <li><a href='https://github.com/pathaynes' target='blank'><FontAwesomeIcon icon={faGithub} /><span>github</span></a></li>
+          <li><a href='https://www.instagram.com/pat_haynes_/' target='blank'><FontAwesomeIcon icon={faInstagram} /><span>instagram</span></a></li>
           <li><a href='mailto:pathaynes32@gmail.com' target='blank'><FontAwesomeIcon icon={faEnvelope} /><span>contact me!</span></a></li>
           <li><a href='https://drive.google.com/file/d/1odQB2uj4L6_D96U9cvZZVHjUDQdZH9Nk/view?usp=sharing' target='blank'><FontAwesomeIcon icon={faFileAlt} /><span>resume</span></a></li>
         </ul>
