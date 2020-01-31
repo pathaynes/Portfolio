9354ad3695bef28f7e8ac177571d6ff062bae0da
Pat Haynes <pathaynes32@gmail.com>
2020-01-31 13:45:32 -0800

    updated some css

--- src/components/About/TechStack.js
+++ src/components/About/TechStack.js
@@ -8,8 +8,8 @@ const TechStack = () => (
       <h2>Tech Stack</h2>
       <div>
         <li>Languages<span>JavaScript, HTML, CSS, Sql</span></li>
-        <li>Front-end<span>React, Redux</span></li>
-        <li>Back-end<span>Node.js, Express</span></li>
+        <li>Frontend<span>React, Redux</span></li>
+        <li>Backend<span>Node.js, Express</span></li>
         <li>Databases<span>Firebase, Mongodb, Mongoose</span></li>
         <li>Practices<span>Agile Development, Ttd, Ddd, Mob/Pair Programming, Clean Code</span></li>
         <li>Design<span>Illustrator, Photoshop</span></li>
