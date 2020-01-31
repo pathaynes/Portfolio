10b0a3b9e242b1177bb0b86b2967b9e2c7470182
Pat Haynes <pathaynes32@gmail.com>
2020-01-30 18:37:41 -0800

    updated carosel with no description

--- src/components/Photography/Photography.js
+++ src/components/Photography/Photography.js
@@ -19,49 +19,39 @@ const Photography = () => {
   return (
     <div className={styles.Photography}>
       <Carousel data-aos="zoom-in" activeIndex={index} direction={direction} onSelect={handleSelect} className={styles.Carousel}>
+        
         <Carousel.Item className={styles.First}>
           <a href='https://crypto-trades-2020.netlify.com/login' target='blank' rel="noopener noreferrer"><img
             className="d-block w-100"
             src={CryptoTrades}
             alt="First slide"
           /></a>
-          <Carousel.Caption>
-            <p>Full Stack application, Fantasy crypto currency trading app, built with React, Redux, Node.js, MongoDB</p>
-          </Carousel.Caption>
         </Carousel.Item>
+
         <Carousel.Item className={styles.Second}>
           <a href='https://github.com/Healing-HeartBot/healing-heart-bot' target='blank' rel="noopener noreferrer"><img
             className="d-block w-100"
             src={heartbot}
             alt="Third slide"
           /></a>
-
-          <Carousel.Caption>
-            <p>Twitter Bot back-end project.</p>
-          </Carousel.Caption>
         </Carousel.Item>
+
         <Carousel.Item className={styles.Third}>
           <a href='https://team-jabba.github.io/html/coverpage.html' target='blank' rel="noopener noreferrer"><img
             className="d-block w-100"
             src={jabbaDice}
             alt="Third slide"
           /></a>
-
-          <Carousel.Caption>
-            <p>Vanilla JavaScript dice game, beat all the bosses to win!.</p>
-          </Carousel.Caption>
         </Carousel.Item>
+
         <Carousel.Item className={styles.Fourth}>
           <a href='https://chroma-game.herokuapp.com/index.html?redirect=%2F' target='blank' rel="noopener noreferrer"><img
             className="d-block w-100"
             src={chroma}
             alt="Third slide"
           /></a>
-
-          <Carousel.Caption>
-            <p>Full-Stack Vanilla JavaScript color matching game to relax you when stressed.</p>
-          </Carousel.Caption>
         </Carousel.Item>
+
       </Carousel>
     </div>
   );
